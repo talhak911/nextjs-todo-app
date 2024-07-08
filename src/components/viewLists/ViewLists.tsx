b447e910b8127416e7e8d23d121fcb59e3d96642
Talha Sadiq <talhak911>
2024-07-08 16:32:43 +0500

    lists with crud

--- src/components/viewLists/ViewLists.tsx
+++ src/components/viewLists/ViewLists.tsx
@@ -1,17 +1,34 @@
 "use client";
 import Link from "next/link";
 import { useViewLists } from "./useViewLists";
+import ListCrud from "../listCrud/ListCrud";
 
 export default function ViewLists() {
-  const { lists } = useViewLists();
-
+  const { lists ,loading} = useViewLists();
+if(loading){
   return (
-    <div className="flex flex-col">
+    <div>
+      <h1>Loading</h1>
+    </div>
+  )
+}
+  return (
+    <div className="flex flex-col gap-2">
       {lists.map((list, index) => {
         return (
-          <Link key={index} href={`/tasks?listId=${list.id}`}>
-            {list.title}
+         <div 
+         key={index} 
+         className=" flex justify-between">
+             <Link 
+           
+            href={`/tasks?listId=${list.id}&listName=${list.title}`} 
+            className="relative text-3xl w-fit"
+          >
+            <span className="relative z-10">{list.title}</span>
+            <span className="absolute left-0 right-0 bottom-2 h-[4px] bg-vintage-garden-accent z-0"></span>
           </Link>
+         <ListCrud update={true} listIdToUpdate = {list.id} listTitle={list.title}/>
+         </div>
         );
       })}
     </div>
