23ce58c5cbf88034f3e77ce533a0a96fd067c8e9
Talha Sadiq <talhak911>
2024-07-08 16:36:58 +0500

    update list function

--- src/redux/slices/listSlice.ts
+++ src/redux/slices/listSlice.ts
@@ -44,12 +44,45 @@ export type AddListType={
   title:string,
   theme:string
 }
+export type UpdateListType={
+  listId:string,
+  title:string,
+  theme:string
+}
 
 
 export const addList = createAsyncThunk
 ("todoSlice/addList", async ({email,title,theme}: AddListType, { rejectWithValue }) => {
  try {
    const response = await axios.post("api/todos/add/list", { email,title,theme });
+   return response.data;
+ } catch (error) {
+   let errorMessage = "An unknown error occurred";
+   if (axios.isAxiosError(error) && error.response) {
+     errorMessage = error.response.data.message || error.message;
+   }
+   return rejectWithValue(errorMessage);
+ }
+});
+export const updateList = createAsyncThunk
+("todoSlice/updateList", async ({listId,title,theme}: UpdateListType, { rejectWithValue }) => {
+ try {
+   const response = await axios.put("api/todos/update/list", { listId,title,theme });
+   return response.data;
+ } catch (error) {
+   let errorMessage = "An unknown error occurred";
+   if (axios.isAxiosError(error) && error.response) {
+     errorMessage = error.response.data.message || error.message;
+   }
+   return rejectWithValue(errorMessage);
+ }
+});
+
+
+export const deleteList = createAsyncThunk
+("todoSlice/deleteList", async ({listId}: {listId:string}, { rejectWithValue }) => {
+ try {
+   const response = await axios.delete("api/todos/delete/list", { data:{listId}});
    return response.data;
  } catch (error) {
    let errorMessage = "An unknown error occurred";
@@ -74,9 +107,11 @@ export const addList = createAsyncThunk
          })
       .addCase(fetchLists.fulfilled, (state, action: PayloadAction<ListType[]>) => {
         state.lists=action.payload
+        state.loading= false
       })
       .addCase(fetchLists.rejected,(state, action) => {
         state.error=action.payload as string
+        state.loading= false
       })
    
 
