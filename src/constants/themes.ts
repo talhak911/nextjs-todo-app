0552306469ee6e0b8f64dd3befcbe89d3898005b
Talha Sadiq <talhak911>
2024-07-09 21:22:46 +0500

    misc

--- src/constants/themes.ts
+++ src/constants/themes.ts
@@ -3,56 +3,115 @@
 //     "Oceanic Serenity"
 // ]
 
-
-export const THEMES =[
-    {
-    name:"Vintage Garden",
+// export const themes1 = {
+//   "Vintage Garden": {
+//     primary: "#2D4659",
+//     background: "#FDFBDA",
+//     accent: "#819F7F",
+//   },
+//   "Cosmic Symphony": {
+//     primary: "#F0EB8D",
+//     background: "#413543",
+//     accent: "#8F43EE",
+//   },
+//   "Rustic Charm": {
+//     primary: "#EA5455",
+//     background: "#F9F5EB",
+//     accent: "#E4BD7D",
+//   },
+//   "Sunset Serenade": {
+//     primary: "#210062",
+//     background: "#009FBD",
+//     accent: "#77037B",
+//   },
+//   "Industrial Chic": {
+//     primary: "#F45050",
+//     background: "#F0F0F0",
+//     accent: "#F9D949",
+//   },
+//   "Blackout Neutrals": {
+//     primary: "#F3EFE0",
+//     background: "#222222",
+//     accent: "#22A39F",
+//   },
+//   "Vibrant Spectrum": {
+//     primary: "#4A0E5C",
+//     background: "#CCF0C3",
+//     accent: "#BCA3CA",
+//   },
+//   "Coastal Sunrise": {
+//     primary: "#005874",
+//     background: "#E6E6D4",
+//     accent: "#FFBE00",
+//   },
+//   "Oceanic Serenity": {
+//     primary: "#CBE4DE",
+//     background: "#2C3333",
+//     accent: "#2E4F4F",
+//   },
+// };
+export const THEMES = [
+  {
+    name: "Vintage Garden",
+    value: "vintageGarden",
     primary: "#2D4659",
     background: "#FDFBDA",
     accent: "#819F7F",
-    },
-    {
-        name: "Cosmic Symphony",
-            primary: "#F0EB8D",
+  },
+  {
+    name: "Cosmic Symphony",
+    value: "cosmicSymphony",
+    primary: "#F0EB8D",
     background: "#413543",
     accent: "#8F43EE",
-    },
-    {
-        name:"Rustic Charm",
+  },
+  {
+    name: "Rustic Charm",
+    value: "rusticCharm",
     primary: "#EA5455",
     background: "#F9F5EB",
     accent: "#E4BD7D",
-    },
-   {name: "Sunset Serenade",
+  },
+  {
+    name: "Sunset Serenade",
+    value: "sunsetSerenade",
     primary: "#210062",
     background: "#009FBD",
     accent: "#77037B",
-    },
-     {name:"Industrial Chic",
+  },
+  {
+    name: "Industrial Chic",
+    value: "industrialChic",
     primary: "#F45050",
     background: "#F0F0F0",
     accent: "#F9D949",
-    },
-    {name: "Blackout Neutrals",
+  },
+  {
+    name: "Blackout Neutrals",
+    value: "blackoutNeutrals",
     primary: "#F3EFE0",
     background: "#222222",
     accent: "#22A39F",
-    },
-     {name:"Vibrant Spectrum",
+  },
+  {
+    name: "Vibrant Spectrum",
+    value: "vibrantSpectrum",
     primary: "#4A0E5C",
     background: "#CCF0C3",
     accent: "#BCA3CA",
-    },
-    {name: "Coastal Sunrise",
+  },
+  {
+    name: "Coastal Sunrise",
+    value: "coastalSunrise",
     primary: "#005874",
     background: "#E6E6D4",
     accent: "#FFBE00",
-    },
-   {name: "Oceanic Serenity",
+  },
+  {
+    name: "Oceanic Serenity",
+    value: "oceanicSerenity",
     primary: "#CBE4DE",
     background: "#2C3333",
     accent: "#2E4F4F",
-    },
+  },
 ];
-
-    
--- src/utils/getDataFromToken.ts
+++ src/utils/getDataFromToken.ts
@@ -1,18 +1,18 @@
-import { NextRequest } from "next/server";
-import { jwtVerify } from 'jose';
+// import { NextRequest } from "next/server";
+// import { jwtVerify } from 'jose';
 
-export const getDataFromToken = async (request: NextRequest): Promise<string> => {
-    try {
-        const token = request.cookies.get("token")?.value || '';
-        if (!token) {
-            throw new Error("Token not found");
-        }
+// export const getDataFromToken = async (request: NextRequest): Promise<string> => {
+//     try {
+//         const token = request.cookies.get("token")?.value || '';
+//         if (!token) {
+//             throw new Error("Token not found");
+//         }
 
-        const secret = new TextEncoder().encode(process.env.TOKEN_SECRET);
-        const { payload } = await jwtVerify(token, secret);
+//         const secret = new TextEncoder().encode(process.env.TOKEN_SECRET);
+//         const { payload } = await jwtVerify(token, secret);
 
-        return payload.id as string;
-    } catch (error: any) {
-        throw new Error(error.message);
-    }
-};
+//         return payload.id as string;
+//     } catch (error: any) {
+//         throw new Error(error.message);
+//     }
+// };
